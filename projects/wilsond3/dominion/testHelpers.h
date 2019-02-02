d4df2bc2f626cde0731e6e6aaef5e9a9b789e61c
Daniel Wilson <seldon.wilson@gmail.com>
2019-02-02 14:44:12 -0800

    Refactored state checking helper functions into their own .c and .h files so they may now be used in other tests. TO DO: update unittest1.c to actually test the shuffle function.

---
+++ projects/wilsond3/dominion/testHelpers.h
@@ -0,0 +1,80 @@
+#ifndef TEST_HELPERS_H
+#define TEST_HELPERS_H
+
+#include <stdbool.h>  // bool
+#include "dominion.h" // struct gameState
+
+typedef struct gameState gameState;
+
+/***********************************************************************************
+* This struct holds the states that could potentially be modified by in-game func- *
+* tions. The values are stored as bit flags in positions shown below.              *
+*                                                                                  *
+*        playerOne/Two/Three/Four |   generalOutpost  |        turn                *
+*        -------------------------+-------------------+--------------------        *
+*            hand...........1     | numPlayers......1 | whoseTurn.........1        *
+*            handCount......2     | supplyCount.....2 | phase.............2        *
+*            deck...........4     | embargoTokens...4 | numActions........4        *
+*            deckCount......8     | outpostPlayed...8 | coins.............8        *
+*            discard.......16     | outPostTurn....16 | numBuys..........16        *
+*            discardCount..32     |                   | playedCards......32        *
+*                                 |                   | playedCardCount..64        *
+*                                                                                  *
+* NOTE: This struct must be initialized with all it's fields set to 0.             *
+*******************************************************************************/
+typedef struct  {   
+   char playerOne;
+   char playerTwo;
+   char playerThree;
+   char playerFour;
+   char turn;
+   char general;
+} ChangedStates;
+
+
+   //  Bit masks to detect mutable states regarding player state properties
+typedef enum {
+   HAND          =  1,
+   HAND_COUNT    =  2,
+   DECK          =  4,
+   DECK_COUNT    =  8,
+   DISCARD       = 16,
+   DISCARD_COUNT = 32
+} PlayerStateFlags;
+
+
+   //  Bit masks to detect mutable states regarding general state properties
+typedef enum {
+   NUM_PLAYERS    =  1,
+   SUPPLY_COUNT   =  2,
+   EMBARGO_TOKENS =  4,
+   OUTPOST_PLAYED =  8,
+   OUTPOST_TURN   = 16
+} GeneralStateFlags;
+
+
+   //  Bit masks to detect mutable states regarding turn state properties
+typedef enum {
+   WHOSE_TURN        =  1,
+   PHASE             =  2,
+   NUM_ACTIONS       =  4,
+   COINS             =  8,
+   NUM_BUYS          = 16,
+   PLAYED_CARDS      = 32,
+   PLAYED_CARD_COUNT = 64
+} TurnStateFlags;
+
+
+   // Determines if only allowable state changes were made
+bool isStateUnchanged(const gameState*, const gameState*, ChangedStates);
+   // Copy all mutable state properties to first argument from second
+gameState* copyStateWithChanges(const gameState*, const gameState*, ChangedStates);
+   // Copy mutable player state properties to first argument from second
+void copyPlayerChanges(gameState*, const gameState*, ChangedStates, int);
+   // Copy mutable turn state properties to first argument from second
+void copyTurnChanges(gameState*, const gameState*, ChangedStates);
+   // Copy mutable general state properties to first argument from second
+void copyGeneralChanges(gameState*, const gameState*, ChangedStates);
+
+
+#endif // TEST_HELPERS_H
